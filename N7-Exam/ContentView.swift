79da21fae4c698f119b6f8805a8c5c828445a4b0
sleep9ull <sleep9ull@gmail.com>
2025-04-20 21:27:20 +0800

    Initial Commit

---
+++ N7-Exam/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  N7-Exam
+//
+//  Created by 斯利普固 on 2025/4/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ N7-Exam/N7_ExamApp.swift
@@ -0,0 +1,17 @@
+//
+//  N7_ExamApp.swift
+//  N7-Exam
+//
+//  Created by 斯利普固 on 2025/4/20.
+//
+
+import SwiftUI
+
+@main
+struct N7_ExamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
